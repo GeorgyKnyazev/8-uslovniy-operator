db6a02aade432f1f08037195e56e5cbe49b65823
Георгий Князев <saint_georgy@yahoo.com>
2022-06-21 22:15:34 +0300

    Добавьте файлы проекта.

---
+++ 8 uslovniy operator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _8_uslovniy_operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretPassword = "123";
+            string userInput;
+            string secretmassege = "секретное сообщение";
+
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine($"Попытка {i}/3");
+                Console.Write("Введите пароль: ");
+                userInput = Console.ReadLine();
+
+                if (userInput == secretPassword)
+                {
+                    Console.WriteLine(secretmassege);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Пароль не верен");
+                }
+            }
+        }
+    }
+}
