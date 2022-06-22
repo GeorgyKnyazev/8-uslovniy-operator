1c6cb08040403993ccfcacea89525711d12815c6
Георгий Князев <saint_georgy@yahoo.com>
2022-06-22 09:44:12 +0300

    2

--- 8 uslovniy operator/Program.cs
+++ 8 uslovniy operator/Program.cs
@@ -7,12 +7,13 @@ namespace _8_uslovniy_operator
         static void Main(string[] args)
         {
             string secretPassword = "123";
+            int numberOfAttempts = 3;
             string userInput;
             string secretmassege = "секретное сообщение";
 
-            for (int i = 1; i <= 3; i++)
+            for (int i = 1; i <= numberOfAttempts; i++)
             {
-                Console.WriteLine($"Попытка {i}/3");
+                Console.WriteLine($"Попытка {i}/{numberOfAttempts}");
                 Console.Write("Введите пароль: ");
                 userInput = Console.ReadLine();
 
